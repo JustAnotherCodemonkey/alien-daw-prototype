20738cc8c2168f10c8db787c930a61c65aa200ce
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-02-24 08:58:24 -0800

    Basic app scaffolding.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use tokio::runtime::Builder;
+
+fn main() -> Result<(), anyhow::Error> {
+    let rt = Builder::new_multi_thread().build()?;
+    let logic_task = rt.spawn(logic());
+
     println!("Hello, world!");
+    Ok(())
 }
+
+async fn logic() {}
