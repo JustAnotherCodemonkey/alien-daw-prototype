28e6968291a93f4d414d81868b7acfa0105ae1d7
Nnanna <omokennanna832@gmail.com>
2024-02-24 19:52:52 +0100

    Drawing a screen!; Removed Cargo.lock from the tree

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
+#![allow(dead_code)]
 use tokio::runtime::Builder;
 use tracing::instrument;
+use winit::{dpi::PhysicalSize, event::*, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder};
 
 fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt().init();
     
     let rt = Builder::new_multi_thread().build()?;
     let logic_task = rt.spawn(logic());
-
+    build_window();
     println!("Hello, world!");
     Ok(())
 }
 
 #[instrument]
 async fn logic() {}
+
+fn build_window() -> (){
+    let evntloop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&evntloop).unwrap();
+    window.set_title("ALIEN DAW");
+    //rerender after every event
+    evntloop.set_control_flow(ControlFlow::Wait);
+
+    let _ = evntloop.run(move|event,elwt|{
+        //match on event
+        match event{
+            //match on events that happen in the window
+            Event::WindowEvent{
+                ref event,
+                window_id: _,
+            } =>  match event{
+                WindowEvent::CloseRequested{} => elwt.exit(),
+                WindowEvent::Resized(PhysicalSize{width: _, height: _ }) => {}//cant find logic to resize the window
+                _ => {}
+            },
+            _ => {}  
+        }
+    }); 
+}
