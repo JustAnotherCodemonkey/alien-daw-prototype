6ea8f0924accf7e6dceed30e5515890aa8f5e33f
Nnanna <omokennanna832@gmail.com>
2024-02-24 20:12:42 +0100

    Clippy Cleaned

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #![allow(dead_code)]
 use tokio::runtime::Builder;
 use tracing::instrument;
-use winit::{dpi::PhysicalSize, event::*, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder};
+use winit::{
+    dpi::PhysicalSize,
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt().init();
-    
+
     let rt = Builder::new_multi_thread().build()?;
-    let logic_task = rt.spawn(logic());
+    let _logic_task = rt.spawn(logic());
     build_window();
     println!("Hello, world!");
     Ok(())
@@ -16,26 +21,37 @@ fn main() -> Result<(), anyhow::Error> {
 #[instrument]
 async fn logic() {}
 
-fn build_window() -> (){
+fn build_window(){
     let evntloop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&evntloop).unwrap();
     window.set_title("ALIEN DAW");
     //rerender after every event
     evntloop.set_control_flow(ControlFlow::Wait);
 
-    let _ = evntloop.run(move|event,elwt|{
+    match evntloop.run(move |event, elwt| {
         //match on event
-        match event{
+        //clippy suggests using if let
+        //there might be more patterns to destructure later
+        //so I'll leave as is
+        match event {
             //match on events that happen in the window
-            Event::WindowEvent{
+            Event::WindowEvent {
                 ref event,
                 window_id: _,
-            } =>  match event{
-                WindowEvent::CloseRequested{} => elwt.exit(),
-                WindowEvent::Resized(PhysicalSize{width: _, height: _ }) => {}//cant find logic to resize the window
+            } => match event {
+                WindowEvent::CloseRequested {} => elwt.exit(),
+                WindowEvent::Resized(PhysicalSize {
+                    width: _,
+                    height: _,
+                }) => {} //cant find logic to resize the window
                 _ => {}
             },
-            _ => {}  
+            _ => {}
         }
-    }); 
+    }) {
+        Ok(()) => {} //event loop runs okay
+        Err(err) => {
+            eprintln!("Error MSG: {}", err)
+        }
+    };
 }
