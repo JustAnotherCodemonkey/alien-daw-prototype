100cb14ae8202743aae365c1c7e30f0926e2ce88
Sophia Waggoner <sophia.waggoner.personal@gmail.com>
2024-02-26 10:00:47 -0800

    Arc-ify CurrentWindowState, use idiomatic struct init syntax for same-name vars/fields, fmt.

--- src/backend.rs
+++ src/backend.rs
@@ -1,13 +1,14 @@
 use crate::*;
+use std::sync::Arc;
 
 //https://sotrh.github.io/learn-wgpu/beginner/tutorial2-surface/#first-some-housekeeping-state
-struct CurrentWindowState <'a>{
-    surface: wgpu::Surface <'a>,
+struct CurrentWindowState {
+    surface: wgpu::Surface<'static>,
     device: wgpu::Device,
     queue: wgpu::Queue,
     configuration: wgpu::SurfaceConfiguration,
     size: PhysicalSize<u32>,
-    window: &'a Window,
+    window: Arc<Window>,
 }
 
 // struct CloneableWindow{
@@ -30,18 +31,17 @@ struct CurrentWindowState <'a>{
 //     }
 // }
 
-impl<'a> CurrentWindowState<'a> {
-    async fn new(window: &'a Window) -> Self {
+impl CurrentWindowState {
+    async fn new(window: Arc<Window>) -> Self {
         let size = window.inner_size();
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             ..Default::default()
         });
-        let surface = wgpu::Instance
-            ::create_surface(&instance, &window)
+        let surface = wgpu::Instance::create_surface(&instance, window.clone())
             .expect("Creation of surface failed!");
         let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptionsBase{
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
                 power_preference: wgpu::PowerPreference::default(),
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
@@ -67,7 +67,7 @@ impl<'a> CurrentWindowState<'a> {
             .filter(|f| f.is_srgb())
             .next()
             .unwrap_or(surface_capabilities.formats[0]);
-        let config = wgpu::SurfaceConfiguration{
+        let configuration = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_fmt,
             width: size.width,
@@ -75,17 +75,17 @@ impl<'a> CurrentWindowState<'a> {
             present_mode: wgpu::PresentMode::Fifo,
             alpha_mode: surface_capabilities.alpha_modes[0],
             view_formats: vec![],
-            desired_maximum_frame_latency: 2
+            desired_maximum_frame_latency: 2,
         };
         // let new_window = CloneableWindow::from_window(window).clone().to_window();
-        return Self{
-            surface: surface,
-            device: device,
-            queue: queue,
-            configuration: config,
-            size: size,
-            window: window
-        }
+        return Self {
+            surface,
+            device,
+            queue,
+            configuration,
+            size,
+            window,
+        };
     }
     fn resize(&mut self, new: PhysicalSize<u32>) {
         todo!()
